8c9fdb75d2193fb315bf2b2af5b504114fe263fc
Karthik Puvvula <karthikpuvvula@Karthiks-MacBook-Air.local>
2021-08-03 00:06:11 +0530

    added cart and cartReducer

--- src/App.js
+++ src/App.js
@@ -1,19 +1,13 @@
 import React from 'react';
-import {
-  Route,
-  Switch
-} from 'react-router-dom';
+import { Route, Switch, Redirect } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
 import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
-import {
-  auth,
-  createUserProfileDocument
-} from './firebase/firebase.utils';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 import { connect } from 'react-redux';
-import { setCurrentUser } from './redux/user/users.actions';
+import { setCurrentUser } from './redux/user/user.actions';
 
 class App extends React.Component {
 
@@ -48,14 +42,18 @@ class App extends React.Component {
           <Switch>
             <Route exact path='/' component={HomePage} />
             <Route path='/shop' component={ShopPage} />
-            <Route path = '/signin' component = { SignInAndSignUp } /> 
+            <Route path ='/signin' exact render = {() => this.props.currentUser ? (<Redirect to='/' />) : (<SignInAndSignUp />) } /> 
           </Switch> 
         </div>);
       }
     }
 
+const mapStateToProps = ({ user }) => ({
+  currentUser: user.currentUser
+});
+
 const mapDispatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
 });
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
